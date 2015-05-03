4ddec1b5815eb08497354f5b85ea220e9329f411
Eliad Cohen <eliadcohen@gmail.com>
2015-05-02 23:04:24 -0400

    Corrected and submitted

--- best.R
+++ best.R
@@ -1,16 +1,15 @@
 best <- function(state, outcome) {
   ## Read outcome data
-  outcomes<-read.csv("outcome-of-care-measures.csv")
+  outcomes<-read.csv("outcome-of-care-measures.csv",colClasses="character")
   possibleoutcomes<-c("heart attack", "heart failure", "pneumonia")
   ## Check that state and outcome are valid
   states<-outcomes[,"State"]
   if (!state%in%outcomes[,"State"]){
-    return(paste("Error in best(",dQuote(state),", ",dQuote(outcome),") : invalid state"))
-  
+    stop("invalid state")  
   }
     
    if (!outcome %in% possibleoutcomes) {
-     return(paste("Error in best(",dQuote(state),", ",dQuote(outcome),") : invalid outcome"))
+     stop("invalid outcome")
      
    }
     if (outcome=="heart attack"){outcome<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"}
@@ -18,9 +17,9 @@ best <- function(state, outcome) {
     if (outcome=="pneumonia"){outcome<-"Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"}
     bystate<-subset(outcomes,State==state,select=c(2,11,17,23))
     bystate<-bystate[order(bystate$Hospital.Name),]
-    bystate[,2]<-as.numeric(bystate[,2])
-    bystate[,3]<-as.numeric(bystate[,3])
-    bystate[,4]<-as.numeric(bystate[,4])
+    suppressWarnings(bystate[,2]<-as.numeric(bystate[,2]))
+    suppressWarnings(bystate[,3]<-as.numeric(bystate[,3]))
+    suppressWarnings(bystate[,4]<-as.numeric(bystate[,4]))
     
     ## Return hospital name in that state with lowest 30-day death
     ## rate
