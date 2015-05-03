154a94cc7368b0923f348df09b7c6c1c74e4ad4b
Eliad Cohen <eliadcohen@gmail.com>
2015-05-03 10:50:20 -0400

    Returning data frame

--- rankall.R
+++ rankall.R
@@ -1,8 +1,33 @@
 rankall <- function(outcome, num = "best") {
   ## Read outcome data
-  
+  outcomes<-read.csv("outcome-of-care-measures.csv",colClasses="character")
   ## Check that state and outcome are valid
+  states<-outcomes[,"State"]
+  if (!state%in%outcomes[,"State"]){
+    stop("invalid state")  
+  }
+  possibleoutcomes<-c("heart attack", "heart failure", "pneumonia")
+  if (!outcome %in% possibleoutcomes) {
+    stop("invalid outcome")
+    
+  }
+  
   ## For each state, find the hospital of the given rank
+  states<-unique(states)
+  states<-sort(states)
+  hnames<-c()
+  source("rankhospital.R")
+  for (st in states){
+    hnames<-c(hnames,rankhospital(st,outcome,num))
+  }
+  
+ 
+  
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
+
+  res<-data.frame(hnames,states)
+  rownames(res)<-states
+  colnames(res)<-c("hospital","state")
+  return(res)
 }
